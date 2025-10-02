849e00ea777f51a44fd7bae20a5344e4cc27ee65
Ayaan-Saifi-1 <ayaansaifi2005@gmail.com>
2025-10-02 12:55:59 +0530

    Initial Commit

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        
+        if(n<0)
+        return 1/helper(x,-((long long)n));
+        return helper(x,n);
+    }
+
+    double helper (double x,long long n)
+    {
+        if(n<=0)
+        return 1;
+        if(n%2!=0)
+        return x*helper(x,n-1);
+        return helper(x*x,n/2);
     }
 };

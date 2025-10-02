759c0e317431c32d48901c5b3f3f42d97b92b22c
hocuspocus07 <150130202+hocuspocus07@users.noreply.github.com>
2025-10-02 13:19:41 +0530

    roman to integer

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        
+        unordered_map<char, int> mp = {{'I', 1},   {'V', 5},   {'X', 10},
+                                       {'L', 50},  {'C', 100}, {'D', 500},
+                                       {'M', 1000}};
+        int n = s.size();
+        int number = 0;
+        for (int i = 0; i < n; i++) {
+            if (i + 1 < n && mp[s[i]] < mp[s[i + 1]]) {
+                number -= mp[s[i]];
+            } else {
+                number += mp[s[i]];
+            }
+        }
+        return number;
     }
 };

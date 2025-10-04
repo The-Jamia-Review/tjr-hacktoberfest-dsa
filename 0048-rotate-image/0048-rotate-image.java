438797094e41fac22f67b299b35978ec6ac199b0
maaz2005-tech <maazzaki2005@gmail.com>
2025-10-04 21:48:59 +0530

    Maaz 🍉 LeetCode 48

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,5 +1,16 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        
+        int n=matrix.length;
+        int[][] rmatrix=new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                rmatrix[i][j]=matrix[n-1-j][i];
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                matrix[i][j]=rmatrix[i][j];
+            }
+        }
     }
 }

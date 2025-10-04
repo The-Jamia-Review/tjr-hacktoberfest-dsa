ffbe142707fba64440e8d25ca912a99088d4c551
maaz2005-tech <maazzaki2005@gmail.com>
2025-10-04 21:41:51 +0530

    Maaz🍉 LeetCode22

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -2,5 +2,37 @@ class Solution
 {
     public List<String> generateParenthesis(int n) 
     {
+        List<String> list=new ArrayList<>();
+    private boolean isValid(String str){
+        Stack<Character> s=new Stack<>();
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)=='('){
+                s.push('(');
+            }
+            else if(!s.isEmpty() && str.charAt(i)==')'){
+                s.pop();
+            }
+            else return false;
+        }
+        return s.isEmpty()==true;
+    }
+    private void solve(StringBuilder sb,int n){
+        if(sb.length()==2*n){
+            String str=sb.toString();
+            if(isValid(str)){
+                list.add(sb.toString());
+            }
+            return ;
+        }
+        sb.append("(");
+        solve(sb,n);
+        sb.deleteCharAt(sb.length()-1);
+        sb.append(")");
+        solve(sb,n);
+        sb.deleteCharAt(sb.length()-1);
+    }
+    public List<String> generateParenthesis(int n) {
+        solve(new StringBuilder(),n);
+        return list;
     }
 }
